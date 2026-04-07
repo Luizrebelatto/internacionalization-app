2801258a2995ebac94fa1bb7e3492e22e236645e
Luiz Gabriel Rebelatto <lbrebelatto@gmail.com>
2026-04-06 22:03:06 -0300

    get language by location

--- app/(tabs)/location.tsx
+++ app/(tabs)/location.tsx
@@ -1,9 +1,55 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect } from "react";
+import { useTranslation } from "react-i18next";
+import { StyleSheet, Text, View } from "react-native";
 
-export default function LocationScreen() {
+import i18n from "../../languages/i18n";
+
+import * as Location from "expo-location";
+
+export default function App() {
+  const { t } = useTranslation();
+
+  const setLanguageByLocation = async (latitude: number, longitude: number) => {
+    try {
+      const geocode = await Location.reverseGeocodeAsync({
+        latitude,
+        longitude,
+      });
+      const country = geocode[0]?.isoCountryCode;
+      const countryToLanguage = {
+        BR: "pt",
+        US: "en",
+        ES: "es",
+      };
+
+      const language = countryToLanguage[country] || "pt";
+      i18n.changeLanguage(language);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLanguageByLocation(
+        location.coords.latitude,
+        location.coords.longitude,
+      );
+    })();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Location</Text>
+      <Text style={styles.greeting}>{t("welcome")}</Text>
+      <Text style={styles.name}>{`${t("name")} Luiz Gabriel`}</Text>
+      <StatusBar style="auto" />
     </View>
   );
 }
@@ -11,11 +57,15 @@ export default function LocationScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  text: {
-    fontSize: 24,
-    fontWeight: 'bold',
+  greeting: {
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  name: {
+    fontSize: 20,
   },
 });
